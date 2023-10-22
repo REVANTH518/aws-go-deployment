ba85662d2bee58164e97c8170ae0ae0823607771
Mallavarapu Revanth <revanth518@gmail.com>
2023-10-22 20:59:16 +0530

    added script files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi"
-docker login -u "revanth518" -p "Rev@nth518" "docker.io"
+
 docker pull revanth518/aws-go-deployment
 
-docker container run -d -p 8000:8080 --name revanth518/aws-go-deployment aws-go-deployment
+docker container run -d -p 8000:8080 --name revanth518/aws-go-deployment
