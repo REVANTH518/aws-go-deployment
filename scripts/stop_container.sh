0088af70d2e24ede2ccb657dd7cc15ca75b4718e
Mallavarapu Revanth <revanth518@gmail.com>
2023-10-22 21:58:22 +0530

    cicd test

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,3 +3,11 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi"
+
+docker stop $(docker ps -a -q)
+
+docker rm -f $(docker ps -a -q)
+
+docker rmi --force $(docker images -a -q | uniq)
+
+echo "Stopped and Removed"
